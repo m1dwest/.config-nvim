8c7efd05159caa9b7e73094223bbd9c90db65f49
Roman Peterburgskii <waveformspb@gmail.com>
2022-12-22 18:01:00 +0300

    Sort plugin list in README

--- packer_plugins.lua
+++ packer_plugins.lua
@@ -8,7 +8,22 @@ local file = io.open("README.md", "w")
 io.output(file)
 io.write(header)
 
-for key, value in pairs(packer_plugins) do
+local function pairsByKeys(t, f)
+    local a = {}
+    for n in pairs(t) do table.insert(a, n) end
+    table.sort(a, f)
+
+    local i = 0
+    local iter = function()
+        i = i + 1
+        if a[i] == nil then return nil
+        else return a[i], t[a[i]]
+        end
+    end
+    return iter
+end
+
+for key, value in pairsByKeys(packer_plugins, function(lhs, rhs) return lhs:lower() < rhs:lower() end) do
     line = string.format("* [%s](%s)\n", key, value.url)
     io.write(line)
 end
