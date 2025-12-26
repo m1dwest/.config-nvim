e0b2a5e2f4c19dee796884c514b993eff9d6a894
Roman Peterburgskii <waveformspb@gmail.com>
2025-12-26 11:04:54 +0900

    Fix todo-comments setup

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,8 @@
 return {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    event = "VeryLazy"
+    config = function()
+        require("todo-comments").setup()
+    end,
+    -- event = "VeryLazy"
 }
