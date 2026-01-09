8a1bd32582dfe480a5da2c5350c0a2ca6588b856
Roman Peterburgskii <waveformspb@gmail.com>
2026-01-09 13:10:08 +0900

    Replace neoscroll with cinnamon

---
+++ lua/plugins/cinnamon.lua
@@ -0,0 +1,15 @@
+return {
+  "declancm/cinnamon.nvim",
+  version = "*", -- use latest release
+    config = function()
+    require("cinnamon").setup({
+        keymaps = {
+            basic = true,
+            extra = true,
+        },
+        options = {
+            delay = 4,
+        }
+    })
+end,
+}
--- lua/plugins/neoscroll.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "karb94/neoscroll.nvim",
-    opts = {
-        hide_cursor = true,
-        duration_multiplier = 0.25,
-    },
-}
