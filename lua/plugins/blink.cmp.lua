51e7998fc168bb6b43c5e1edb5453498c1bbc0dd
Roman Peterburgskii <waveformspb@gmail.com>
2026-03-11 14:39:27 +0900

    Change keymap preset for Blink.cmp

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -26,7 +26,9 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'enter',
+        },
 
         appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -35,7 +37,15 @@ return {
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = true } },
+        completion = {
+            menu = {
+                border = "single"
+            },
+            documentation = {
+                auto_show = true,
+                window = { border = "single" },
+            }
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
