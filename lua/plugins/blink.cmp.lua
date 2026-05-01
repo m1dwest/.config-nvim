dc47ef26db5e232fa34a57ebd002fb242cd30334
Roman Peterburgskii <waveformspb@gmail.com>
2026-05-01 09:10:09 +0900

    Add blink.lib, remove cinnamon

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -2,14 +2,9 @@ return {
     'saghen/blink.cmp',
 
     dependencies = { 'rafamadriz/friendly-snippets' },
-    build = 'cargo build --release',
-
-    -- use a release tag to download pre-built binaries
-    -- version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    build = function()
+        require('blink.cmp').build():wait(60000)
+    end,
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -36,7 +31,6 @@ return {
             nerd_font_variant = 'mono'
         },
 
-        -- (Default) Only show the documentation popup when manually triggered
         completion = {
             menu = {
                 border = "single"
@@ -47,18 +41,13 @@ return {
             }
         },
 
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
 
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+        fuzzy = {
+            implementation = "rust"
+        }
     },
     opts_extend = { "sources.default" }
 }
